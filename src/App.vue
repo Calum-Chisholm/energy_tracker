cb87d8356d55deb620b6233915a0a86b7a546ac7
CallumGrindle <callumgrindle@gmail.com>
2019-10-17 13:10:12 +0100

    api call working

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <template lang="html">
-
+  <h1></h1>
 </template>
 
 <script>
 export default {
+  name: 'app',
+  data() {
+    return {
+      energyStats: []
+    }
+  },
+  mounted(){
+    fetch('https://api.carbonintensity.org.uk/generation')
+    .then(res => res.json())
+    .then(data => this.energyStats = data);
+  }
 }
 </script>
 
---
+++ src/components/enerygyDisplay.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+
+</template>
+
+<script>
+import { GChart } from 'vue-google-charts'
+
+export default {
+  components: {
+    GChart
+  }
+}
+
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+</style>
